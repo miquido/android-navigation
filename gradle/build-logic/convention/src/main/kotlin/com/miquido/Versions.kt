2e75f2c9e22400d4d14cd43008ead70b25ff18a0
Roman Savka <roman.savka@miquido.com>
2024-07-24 09:48:11 +0200

    Version 1.0.0

--- gradle/build-logic/convention/src/main/kotlin/com/miquido/Versions.kt
+++ gradle/build-logic/convention/src/main/kotlin/com/miquido/Versions.kt
@@ -1,6 +1,6 @@
 package com.miquido
 
-const val projectVersionName = "1.0.0-beta"
+const val projectVersionName = "1.0.0"
 const val projectCompileSdk = 34
 const val projectMinSdk = 23
 const val projectTargetSdk = 34
